b20dc06a4e217f2e4d9c45b328d3cc9d5964bb60
Roman Lomakin <cool.spectre2017@yandex.ru>
2021-03-11 16:17:50 +0300

    p

--- js/mapbox-print-pdf.js
+++ js/mapbox-print-pdf.js
@@ -94,7 +94,7 @@ var PdfBuilder = (function() {
                     var renderFormat = getRenderFormat(format, orientation, convMargins);
                     try { // convMargins.left(), convMargins.top()
                         console.log(dimensions.width())
-                        pdf.addImage(canvas.toDataURL('image/jpeg', 1), 'JPEG', convMargins.left(), convMargins.top(),
+                        pdf.addImage(canvas.toDataURL('image/jpeg', 1), 'JPEG', -1, -1,
                             renderFormat.width(),
                             renderFormat.height(), null, 'FAST');
                         resolve(pdf);
